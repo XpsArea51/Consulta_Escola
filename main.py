084cf0393c8703d148f3ee4c485ee5da52b824eb
Lucas Gabriel <117945122+lucasleirbag@users.noreply.github.com>
2023-12-07 09:28:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -83,8 +83,7 @@ class SchoolWidget(QtWidgets.QWidget):
         self.show_map_button.clicked.connect(self.open_map)
         layout.addWidget(self.show_map_button)
 
-        layout.addSpacing(20)  # Adiciona espaço entre os widgets de escola
-
+        layout.addSpacing(20) 
 
     def open_map(self):
         if pd.isna(self.latitude) or pd.isna(self.longitude):
@@ -109,7 +108,6 @@ class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        # Define arquivos CSV
         self.csv_files = [csv_censo_path]
         
         self.load_data()
@@ -119,7 +117,7 @@ class App(QtWidgets.QWidget):
         for file in self.csv_files:
             if os.path.exists(file):
                 self.df = pd.read_csv(file, delimiter=';', dtype={'Código INEP': str})
-                self.df['UF'] = self.df['UF'].astype(str)  # Convert 'UF' column to string
+                self.df['UF'] = self.df['UF'].astype(str)
             else:
                 QtWidgets.QMessageBox.critical(self, 'Erro', 'Arquivo CSV não encontrado')
                 if hasattr(self, 'button_codigo') and hasattr(self, 'button_nome'):
@@ -131,24 +129,20 @@ class App(QtWidgets.QWidget):
     def init_ui(self):
 
         self.setWindowTitle('Busca Inteligente de Escolas')
-        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)  # permite minimizar e fechar a janela
-        self.setGeometry(750, 300, 500, 550)  # define o tamanho e a posição inicial da janela
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
+        self.setGeometry(750, 300, 500, 550)  
 
-        self.setWindowIcon(QIcon(icone_path))  # Adiciona um ícone à janela
+        self.setWindowIcon(QIcon(icone_path))
 
-        # layout vertical
         layout = QtWidgets.QVBoxLayout(self)
 
-        # campo de entrada
         self.entry_label = QLabel("Código Censo ou Nome da IE:", self)
         layout.addWidget(self.entry_label)
         self.entry = QtWidgets.QLineEdit(self)
         layout.addWidget(self.entry)
 
-        # Definir a largura desejada para a combobox
         largura_desejada = 100
 
-        # Campo de entrada para a UF
         self.uf_label = QLabel("Selecione a UF:", self)
         layout.addWidget(self.uf_label)
 
@@ -156,26 +150,21 @@ class App(QtWidgets.QWidget):
         self.uf_entry.addItems(sorted(['Todos'] + list(self.uf_mapping.values())))
         self.uf_entry.setCurrentText('Todos')
 
-        # Definir a largura da combobox
         self.uf_entry.setFixedWidth(largura_desejada)
 
         layout.addWidget(self.uf_entry)
 
-        # cria uma lista de UFs únicas a partir da planilha
         self.ufs = self.df['UF'].unique().tolist()
 
-        # adiciona todas as formas possíveis de entrada para a UF à lista de UFs
         self.ufs += [uf.lower() for uf in self.ufs] + list(self.uf_mapping.keys())
 
-        # Configurando o autocompletar para a entrada de texto da UF
         self.uf_completer = QCompleter(self.ufs, self)
         self.uf_entry.setCompleter(self.uf_completer)
 
-        # botões
         button_layout = QtWidgets.QHBoxLayout()
 
         self.button_codigo = QPushButton('Buscar código censo', self)
-        self.button_codigo.setFixedSize(250, 45)  # Ajuste esses números para o tamanho que você deseja
+        self.button_codigo.setFixedSize(250, 45)
         self.button_codigo.setStyleSheet("""
             QPushButton {
                 color: #FFF;
@@ -195,7 +184,7 @@ class App(QtWidgets.QWidget):
         self.button_codigo.clicked.connect(self.buscar_codigo)
 
         self.button_nome = QPushButton('Buscar Nome', self)
-        self.button_nome.setFixedSize(250, 45)  # Ajuste esses números para o tamanho que você deseja
+        self.button_nome.setFixedSize(250, 45) 
         self.button_nome.setStyleSheet("""
             QPushButton {
                 color: #FFF;
@@ -216,7 +205,6 @@ class App(QtWidgets.QWidget):
 
         layout.addLayout(button_layout)
 
-        # labels para o nome e o endereço da escola
         self.nome_label = QLabel("", self)
         self.nome_label.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
         layout.addWidget(self.nome_label)
@@ -229,7 +217,7 @@ class App(QtWidgets.QWidget):
 
         self.button = QtWidgets.QPushButton("Travar Janela", self)
         self.button.setFixedSize(115, 22)
-        self.button.setCheckable(True)  # torna o botão toggle
+        self.button.setCheckable(True)
         self.button.setStyleSheet("""
             QPushButton {
                 background-color: #333;
@@ -251,12 +239,10 @@ class App(QtWidgets.QWidget):
             }
         """)
 
-        # Conecta o sinal 'clicked' ou 'toggled' ao slot 'toggle_topmost'
         self.button.toggled.connect(self.toggle_topmost)
 
         layout.addWidget(self.button)
 
-        # scroll area
         self.scroll_area = QtWidgets.QScrollArea(self)
         self.scroll_area.setWidgetResizable(True)
         layout.addWidget(self.scroll_area)
@@ -265,7 +251,6 @@ class App(QtWidgets.QWidget):
         self.scroll_layout = QtWidgets.QVBoxLayout(self.scroll_content)
         self.scroll_area.setWidget(self.scroll_content)
 
-        # Assinatura
         self.signature_label = QLabel("Desenvolvido por Lucas Gabriel©", self)
         self.signature_label.setStyleSheet("color: gray;")
         self.signature_label.setAlignment(QtCore.Qt.AlignCenter)
@@ -289,12 +274,10 @@ class App(QtWidgets.QWidget):
     def buscar_codigo(self):
         codigo_censo = self.entry.text()
         
-        # Remove todos os widgets anteriores
         self.clear_widgets()
 
         rows = self.df[self.df['Código INEP'].astype(str) == codigo_censo]
         if not rows.empty:
-            # Adiciona os novos widgets
             for index, row in rows.iterrows():
                 school_widget = SchoolWidget(row['Escola'], row['Endereço'], row['Código INEP'], row['Latitude'], row['Longitude'], row['Município'], row['UF'])
                 self.scroll_layout.addWidget(school_widget)
@@ -307,19 +290,16 @@ class App(QtWidgets.QWidget):
         nome_escola = self.entry.text().lower()
         uf = self.uf_entry.currentText().upper()
 
-        # Validação dos dados de entrada
         if not nome_escola:
             QtWidgets.QMessageBox.critical(self, 'Erro', 'Nome da escola é inválido')
             return
 
-        # Remove acentos, caracteres especiais e converte para minúsculas
+        
         self.df['Escola_normalized'] = self.df['Escola'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8').str.lower()
 
-        # Busca a escola pelo nome normalizado e pela UF
         words = nome_escola.split()
-        query = ' & '.join(f'Escola_normalized.str.contains("{word}")' for word in words)  # join the words with AND operator
+        query = ' & '.join(f'Escola_normalized.str.contains("{word}")' for word in words)  
         
-        # Se a UF não for 'Todos', adicione isso à consulta
         if uf != 'TODOS':
             query += f' and UF == "{uf}"'
 
@@ -331,30 +311,25 @@ class App(QtWidgets.QWidget):
             QtWidgets.QMessageBox.critical(self, 'Erro', 'Nenhuma escola encontrada')
             return
 
-        # Remove todos os widgets anteriores
         self.clear_widgets()
-
-        # Adiciona os novos widgets
+        
         for _, row in rows.iterrows():
             school_widget = SchoolWidget(row['Escola'], row['Endereço'], row['Código INEP'], row['Latitude'], row['Longitude'], row['Município'], row['UF'])
             self.scroll_layout.addWidget(school_widget)
 
 
     def clear_widgets(self):
-        # Remove todos os widgets anteriores
+        
         for i in reversed(range(self.scroll_layout.count())):
             widgetToRemove = self.scroll_layout.itemAt(i).widget()
-            # remove it from the layout list
             self.scroll_layout.removeWidget(widgetToRemove)
-            # remove it from the gui
             widgetToRemove.setParent(None)
 
     def abrir_localizacao(self):
         nome_escola = self.entry.text().lower()
         uf = self.uf_entry.currentText().upper()
-        municipio = 'SEU_MUNICIPIO'  # Substitua pelo município adequado
+        municipio = 'SEU_MUNICIPIO' 
 
-        # Validação dos dados de entrada
         if not nome_escola or not uf:
             QtWidgets.QMessageBox.critical(self, 'Erro', 'Nome da escola ou UF inválidos')
             return
@@ -365,10 +340,8 @@ class App(QtWidgets.QWidget):
             QtWidgets.QMessageBox.critical(self, 'Erro', 'Nenhuma escola encontrada')
             return
 
-        # Remove todos os widgets anteriores
         self.clear_widgets()
 
-        # Adiciona os novos widgets
         for _, row in rows.iterrows():
             school_widget = SchoolWidget(row['Escola'], row['Endereço'], row['Código INEP'], row['Latitude'], row['Longitude'], row['Município'], row['UF'])
             self.scroll_layout.addWidget(school_widget)
@@ -384,8 +357,7 @@ class App(QtWidgets.QWidget):
 
 app = QtWidgets.QApplication([])
 
-# Defina o pixmap para o splash screen e exiba-o
-splash_pix = QPixmap(icone_path)  # Coloque aqui o caminho para a sua logo
+splash_pix = QPixmap(icone_path) 
 splash = QSplashScreen(splash_pix)
 splash.show()
 
@@ -396,6 +368,6 @@ while not window.isVisible():
     app.processEvents()
     time.sleep(0.1)
 
-splash.finish(window)  # Fechar o splash screen
+splash.finish(window) 
 
 app.exec_()
